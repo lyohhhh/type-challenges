97eb8d23cad07d0aaf95c459a925f5ea9c2b6ae3
lyohhhh <lyohhhh@163.com>
2022-04-24 11:51:41 +0800

    docs: add 8-medium-readonly-2 docs

--- questions/8-medium-readonly-2/template.ts
+++ questions/8-medium-readonly-2/template.ts
@@ -3,3 +3,23 @@ type MyReadonly2<T, K extends keyof T = keyof T> = {
 } & {
   readonly [U in K]: T[U];
 };
+
+// TIP:
+// interface Todo1 {
+//   title: string
+//   description?: string
+//   completed: boolean
+// }
+
+// 只有一个泛型时
+// K extends keyof T = keyof T
+// =>
+//  k = {
+//   title: string
+//   description?: string
+//   completed: boolean
+// }
+
+// 1.遍历 T 添加约束 U 是 K 的子级
+// 2.遍历 K 添加 readonly 关键字
+// 3.& => 取 1.2 的交集
