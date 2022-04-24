0fa6e90731517c25fcd13e3ebc83cb3927785cd2
lyohhhh <lyohhhh@163.com>
2022-04-24 14:47:07 +0800

    fix: 修改一个key只能添加一次

--- questions/12-medium-chainable-options/template.ts
+++ questions/12-medium-chainable-options/template.ts
@@ -1,4 +1,7 @@
 type Chainable<T extends {} = {}> = {
-  option<K extends string, V>(key: K, value: V): Chainable<T & { [k in K]: V }>;
+  option<K extends string, V>(
+    key: K extends keyof T ? never : K,
+    value: V
+  ): Chainable<T & { [k in K]: V }>;
   get(): T;
 };
