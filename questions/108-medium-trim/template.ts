f7e1f547a18327c8bd362b446682f36da8cd1623
lyohhhh <lyohhhh@163.com>
2022-04-25 10:09:04 +0800

    perf: 优化 108-medium-trim answer

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,7 +1,5 @@
-type Trim<S extends string> = S extends `${" " | "\n" | "\t"}${infer L}`
-  ? L extends `${infer R}${" " | "\n" | "\t"}`
-    ? Trim<R>
-    : Trim<L>
-  : S extends `${infer R}${" " | "\n" | "\t"}`
+type Trim<S extends string> = S extends
+  | `${" " | "\n" | "\t"}${infer R}`
+  | `${infer R}${" " | "\n" | "\t"}`
   ? Trim<R>
   : S;
